57bf851115aa43775ca07d4777783ac87af64dc6
mariosrb <mario-2000@hotmail.fr>
2025-01-09 17:39:11 +0100

    .

--- push_swap.h
+++ push_swap.h
@@ -10,6 +10,13 @@ int		is_double(char **strs, int i);
 int		is_space(char *str);
 int		is_valid_number(char *str);
 void	free_matrice(char **strs);
+void	reverse_rotate_both(t_list **head_a, t_list **head_b);
+void	reverse_rotate(t_list **head);
+void	rotate_both(t_list **head_a, t_list **head_b);
+void	rotate_to_last(t_list **head);
+void	push_first(t_list **before, t_list **after);
+void	swap_both(t_list **head_a, t_list **head_b);
+void	swap_first(t_list **head);
 
 
 #endif
